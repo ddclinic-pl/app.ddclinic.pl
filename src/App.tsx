5218ca6e2eef1811b71c6600d14a9e2b65263421
Bartosz Galek <bartosz@galek.com.pl>
2025-03-22 21:14:35 +0100

    initial version

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import { AppShell, Burger, Group, Skeleton, Image } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import logo from "../public/logo.svg";
+
+export default function App() {
+  const [opened, { toggle }] = useDisclosure();
+  return (
+    <AppShell
+      header={{ height: 60 }}
+      navbar={{ width: 300, breakpoint: "sm", collapsed: { mobile: !opened } }}
+      padding="md"
+    >
+      <AppShell.Header>
+        <Group h="100%" px="md">
+          <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
+          <Image src={logo} height={50} />
+        </Group>
+      </AppShell.Header>
+      <AppShell.Navbar p="md">
+        Navbar
+        {Array(15)
+          .fill(0)
+          .map((_, index) => (
+            <Skeleton key={index} h={28} mt="sm" animate={false} />
+          ))}
+      </AppShell.Navbar>
+      <AppShell.Main>Main</AppShell.Main>
+    </AppShell>
+  );
+}
